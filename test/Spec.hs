a2912e163325057a3638501ba9cf0ad6253d4e5a
Paolo Veronelli <paolo.veronelli@gmail.com>
2020-01-10 15:40:16 +0100

    remove golden test creation from IO

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE OverloadedStrings, NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedStrings, NoMonomorphismRestriction, NoImplicitPrelude #-}
 
 
 import qualified Data.ByteString.Char8 as BS
@@ -9,6 +9,7 @@ import System.FilePath.Glob
 
 import Test.Tasty
 import Test.Tasty.Golden as G
+import Test.Tasty.Golden.Advanced as G
 
 import Pipes.XML
 import Pipes.XML.Token
@@ -16,36 +17,48 @@ import qualified Pipes.Prelude as P
 import Pipes
 import Pipes.Core
 import Text.Pretty.Simple
-import Protolude
+import Protolude hiding (to)
 import qualified Data.Text as T
-
+import qualified Data.Text.Encoding as T
+import Control.Lens
+import Data.Attoparsec.ByteString.Char8
+import qualified Pipes.Prelude as P
 
 listTestFiles :: FilePath -> IO [FilePath]
 listTestFiles t = globDir1 pat $ "test/tests" </> t
   where pat = compile "*.xml"
 
-mkGoldenTest :: Show a => Pipe ByteString a IO () 
+
+testPipe 
+    :: Show a 
+    => Pipe Token a IO () 
     -> Text
     -> FilePath 
-    -> IO TestTree
-mkGoldenTest test name path = do
-    let testName   =  takeBaseName path <> ": " <> toS name
-        goldenPath = replaceExtension path $ "." 
-            <> toS (T.replace " " "_" name) 
-            <> ".golden"
-    pure $ goldenVsString
-        testName
-        goldenPath
-        do
-            v      <- BS.readFile path
-            actual <- P.toListM $ yield v >-> test
-            return $ toS $ pShowNoColor actual
+    -> TestTree 
+testPipe test name input = goldenTest testName
+    do BS.readFile goldenPath
+    do
+        v      <- BS.readFile input
+        actual <- P.toListM $ yield v >-> produceTokens >-> test
+        return $ toS $ pShowNoColor actual
+    do \g t -> if g == t then pure Nothing 
+                else do 
+                    BS.writeFile failedPath t
+                    pure $ Just ("failed: " <> failedPath)
+    do BS.writeFile goldenPath 
+    where   testName   =  takeBaseName input <> ": " <> toS name
+            goldenPath = replaceExtension input $ "." 
+                <> toS (T.replace " " "_" name) 
+                <> ".golden"
+            failedPath = replaceExtension input $ "." 
+                <> toS (T.replace " " "_" name) 
+                <> ".failed"
 
-testFile n t f = mkGoldenTest f n $ "test/tests" </>  t
-testFiles n t f =  listTestFiles t  >>=  traverse (mkGoldenTest f n)
+testFile n t f = testPipe f n $ "test/tests" </>  t
+testFiles n ts f =  map (\t -> testPipe f n t) ts
 
-onPlants n f = testFile n "dsl/plant_catalog.xml"  
-        do produceTokens >-> f
+onPlants n = testFile n "dsl/plant_catalog.xml"  
+
 
 select_plants :: Functor m =>Pipe Token ByteString m ()
 select_plants = void $ unPipe $ do
@@ -65,26 +78,36 @@ select_1_plant = void $ unPipe $ do
 
 select_3rd_plant :: Functor m =>Pipe Token ByteString m ()
 select_3rd_plant = void $ unPipe $ do
-    tag "CATALOG"
+    -- tag "CATALOG"
     stimes 2 (tag "PLANT" >> stop)
         <> do
             tag "COMMON"
             pipe $ getText Just //> yield
             stop
 
+select_zone4_plants :: Functor m =>Pipe Token (Either Text Int) m ()
+select_zone4_plants = void $ unPipe $ do
+    -- tag "CATALOG"
+    tags "PLANT"
+    mconcat 
+        [ do tag "COMMON" 
+             pipe $ getText (preview $ to T.decodeUtf8' . _Right) //> yield . Left 
+             stop
+        , do tag "ZONE"
+             pipe $ getText (preview $ to (parseOnly decimal) . _Right) //> yield . Right
+             stop
+        ]
 
 main :: IO ()
 main = do
-    tokenizer <- testFiles "tokenizer" "tokenizer" produceTokens 
-    multi_select <- onPlants  "multi select" select_plants 
-    single_select <- onPlants "single select" select_1_plant
-    pick_plant <- onPlants "3rd select" select_3rd_plant
+    tokenizer_files <- listTestFiles "tokenizer"
     defaultMain 
         do testGroup "Tests"
-            [ do testGroup "Tokenizer" tokenizer
+            [ do testGroup "Tokenizer" 
+                $ testFiles "tokenizer" tokenizer_files cat
             , do testGroup "DSL" 
-                [ single_select
-                , multi_select
-                , pick_plant
-                ]
+                        [ onPlants "multi select" select_plants
+                        , onPlants "single select" select_1_plant
+                        , onPlants "3rd select" select_3rd_plant
+                        ]
             ]
