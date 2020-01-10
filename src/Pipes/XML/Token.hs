52ac673f15788bca11d49e4041753a9703d4b57d
Paolo Veronelli <paolo.veronelli@gmail.com>
2020-01-10 20:56:47 +0100

    fix preproc parsing ad self contained nodes

--- src/Pipes/XML/Token.hs
+++ src/Pipes/XML/Token.hs
@@ -44,6 +44,7 @@ data Token = Tin Text -- ^ tag starts
            | Ttext ByteString -- ^ some text
            | Tcdata ByteString -- ^ some CDATA -- not implemented
            | Tcomment Text
+           | Tpreproc Text ByteString
            | Tdtd Text (Either ByteString [ByteString])
     deriving Show
 
@@ -92,6 +93,18 @@ parseToken Outside = msum
           name <- parseString '>' <* char '>'
           pure ( Outside, pure $ Tout $ T.strip $ toS name )
     , do
+          string "<!--"
+          skipSpace
+          c <- T.decodeUtf8 <$> parseStringS "-->" 
+          pure ( Outside, pure $ Tcomment c)
+    , do
+          string "<?"
+          skipSpace
+          n <- T.decodeUtf8 <$> parseString ' '
+          skipSpace
+          c <- parseStringS "?>" 
+          pure ( Outside, pure $ Tpreproc n c)
+    , do
           string "<!"
           skipSpace
           string "DOCTYPE"
@@ -104,16 +117,14 @@ parseToken Outside = msum
           pure (Outside, pure $ Tdtd n xs)
 
     , do
-          string "<!--"
+          char '<' 
           skipSpace
-          c <- T.decodeUtf8 <$> parseStringS "-->" 
-          pure ( Outside, pure $ Tcomment c)
-    , do
-          () <$ char '<' <|> () <$ string "<?"
-          skipSpace
-          name <- T.strip . T.decodeUtf8 <$> takeWhile1
-              (\x -> x /= ' ' && x /= '>')
-          pure ( Attring name, pure $ Tin name )
+          (n, d) <- parseStringP (0 <$ char ' ' <|> 1 <$ char  '>' <|> 2 <$ string "/>")
+          let name = T.decodeUtf8 n
+          case d of
+              0 -> pure ( Attring name, pure $ Tin name )
+              1 -> pure ( Outside, [Tin name, TinC name] )
+              2 -> pure ( Outside, [Tin name, TinC name, Tout name])
     , do
           t <- parseString ('<')
           guard $ B.length t > 0
@@ -157,15 +168,18 @@ amp = do
 character :: Char -> Parser Char
 character c = amp <|>  escape <|> satisfy (/= c)
 
-characterS :: ByteString -> Parser [Char]
-characterS c = pure <$> amp <|>  pure <$> escape <|>  mempty <$ string c
+characterS :: Parser a -> Parser ([Char],a)
+characterS c = (mempty,) <$> c 
+    <|>  first . (:) <$>  (amp <|>  escape <|>  anyChar) <*> characterS c
 
 parseString :: Char -> Parser ByteString
 parseString c = B.pack <$> many (character c)
 
 parseStringS :: ByteString -> Parser ByteString 
-parseStringS c = B.pack . concat <$> many (characterS c)
+parseStringS c = fst  <$> parseStringP (string c)
 
+parseStringP :: Parser a -> Parser (ByteString, a)
+parseStringP c = first B.pack  <$> characterS c
 
 parseQuoted :: Parser ByteString
 parseQuoted = do
