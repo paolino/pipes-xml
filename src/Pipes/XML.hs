e167624635f5203f7754be1ca79109f0f89aeb98
Paolo Veronelli <paolo.veronelli@gmail.com>
2020-07-17 16:08:19 +0200

    formatting hlinting

--- src/Pipes/XML.hs
+++ src/Pipes/XML.hs
@@ -1,91 +1,84 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE BlockArguments            #-}
-{-# LANGUAGE DataKinds                 #-}
-{-# LANGUAGE DeriveFunctor             #-}
-{-# LANGUAGE FlexibleContexts          #-}
-{-# LANGUAGE GADTs                     #-}
-{-# LANGUAGE NoImplicitPrelude         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
-{-# LANGUAGE OverloadedStrings         #-}
-{-# LANGUAGE ScopedTypeVariables       #-}
-{-# LANGUAGE TypeFamilies              #-}
 
-module Pipes.XML where 
+module Pipes.XML where
 
-import           Control.Arrow                  ( (***) )
-import           Control.Lens            hiding ( deep
-                                                , none
-                                                )
-import           Control.Lens.Extras
+import Control.Lens hiding
+  ( deep,
+    none,
+  )
 import Control.Monad.Cont
-import qualified Data.ByteString.Lazy          as BL
-import qualified Data.ByteString               as B
-import           Data.Map                       ( Map, insert )
-import           Protolude               
-import           Pipes
-import           Pipes.Core
-import           Pipes.Safe              hiding ( handle )
-
+import Data.Map (insert)
+import Pipes
 import Pipes.XML.Token
+import Protolude
 
 -- | a pipe which breaks, and return the breaking token
---
 breakP :: Functor m => (a -> Bool) -> Pipe a a m a
 breakP f = do
-    x <- await 
-    if f x then pure x else yield x >> breakP f
+  x <- await
+  if f x then pure x else yield x >> breakP f
 
 -- | node attributes
 type Attrs = Map Text ByteString
 
 -- | consume all attrs of a node
 getAttrs :: Functor m => Pipe Token a m Attrs
-getAttrs = go mempty 
+getAttrs = go mempty
   where
     go m = do
-        x <- await
-        case x of
-            Tattr k v -> go $ insert k v m
-            TinC _    -> pure m
-            x         -> panic $ "unterminated node attrs: " <> show x
+      x <- await
+      case x of
+        Tattr k v -> go $ insert k v m
+        TinC _ -> pure m
+        y -> panic $ "unterminated node attrs: " <> show y
 
 -- | condition of pipe repetition
-data Loop = Loop | Stop 
+data Loop = Loop | Stop
 
-instance Semigroup Loop where  
-    Loop <> Loop = Loop
-    _ <> _ = Stop
+instance Semigroup Loop where
+  Loop <> Loop = Loop
+  _ <> _ = Stop
 
 instance Monoid Loop where
-    mempty = Loop
+  mempty = Loop
 
 -- | euler scanner, suspend after a tag opening named as the first argument
 -- the second argument is a Pipe to receive the tag internal tokens
 -- TODO check out relative depth is 0 before decide the Tout is correct for bail out
-insideTag
-    :: Functor m
-    => Text
-    -> (Attrs -> Pipe Token a m Loop)
-    -> Pipe Token a m Loop
-insideTag t inside = do
-    x <- await
-    case x of
-        Tin c -> if c == t
-            then  do
-                m <- getAttrs
-                (>->)
-                    do  breakP (\x -> x ^? _Tout == Just c) >> pure mempty
-                    do  let r Loop   = inside m >>= r 
-                            r Stop   = forever await
-                        r Loop
-                        
-            else insideTag t inside
-        _ -> insideTag t inside
-
+insideTag ::
+  Functor m =>
+  Text ->
+  (Attrs -> Pipe Token a m Loop) ->
+  Pipe Token a m Loop
+insideTag t inside' = do
+  x <- await
+  case x of
+    Tin c ->
+      if c == t
+        then do
+          m <- getAttrs
+          (>->)
+            do breakP (\y -> y ^? _Tout == Just c) >> pure mempty
+            do
+              let r Loop = inside' m >>= r
+                  r Stop = forever await
+              r Loop
+        else insideTag t inside'
+    _ -> insideTag t inside'
 
 -- | dsl to flatten the nested pipe structure reflecting xml structure
 newtype CPipe a b m x = CPipe (ContT Loop (Pipe a b m) x)
-    deriving ( Monad, Applicative, Functor, MonadCont )
+  deriving (Monad, Applicative, Functor, MonadCont)
 
 -- | interpret the resulting pipe
 renderPipe :: Functor m => CPipe a b m Loop -> Pipe a b m Loop
@@ -95,8 +88,8 @@ renderPipe (CPipe f) = runContT f return
 pipe :: Functor m => Pipe a b m x -> CPipe a b m x
 pipe = CPipe . lift
 
-instance MonadTrans (CPipe a b ) where
-    lift = pipe . lift 
+instance MonadTrans (CPipe a b) where
+  lift = pipe . lift
 
 -- | declare non repetition of the run pipe
 stop :: Functor m => CPipe a b m Loop
@@ -111,32 +104,30 @@ takeP :: Functor m => Int -> CPipe a b m Loop -> CPipe a b m Loop
 takeP n = fold . replicate n
 
 -- | consume next tag matching
-tag :: Functor m =>  Text -> CPipe Token a m Attrs
+tag :: Functor m => Text -> CPipe Token a m Attrs
 tag b = CPipe $ ContT $ \m -> insideTag b m >> pure Stop
 
 -- | consume all tags matching
-tags :: Functor m =>  Text -> CPipe Token a m Attrs
+tags :: Functor m => Text -> CPipe Token a m Attrs
 tags b = CPipe $ ContT $ \m -> insideTag b m >> pure Loop
 
 instance (Functor m, Semigroup x) => Semigroup (CPipe a b m x) where
-    CPipe (ContT f) <> CPipe (ContT g) = CPipe $ ContT $ \c -> (<>) <$> f c <*> g c 
+  CPipe (ContT f) <> CPipe (ContT g) = CPipe $ ContT $ \c -> (<>) <$> f c <*> g c
 
 instance (Functor m, Monoid x) => Monoid (CPipe a b m x) where
-    mempty = pure mempty
+  mempty = pure mempty
 
 -- | try to consume and parse next text
 getText :: Functor m => (ByteString -> Maybe a) -> Pipe Token a m ()
 getText f = do
-    let p c = case f c of
-            Just x  -> yield x
-            Nothing -> pure ()
-    t <- await
-    case t of
-        Ttext c -> p c 
-        -- Tcdata c -> p c 
-        _ -> getText f
-    
-produceTokens :: Functor m => Pipe ByteString Token m ()
-produceTokens =  chunking Outside parseToken 
- 
+  let p c = case f c of
+        Just x -> yield x
+        Nothing -> pure ()
+  t <- await
+  case t of
+    Ttext c -> p c
+    -- Tcdata c -> p c
+    _ -> getText f
 
+produceTokens :: Functor m => Pipe ByteString Token m ()
+produceTokens = chunking Outside parseToken
--- src/Pipes/XML/Token.hs
+++ src/Pipes/XML/Token.hs
@@ -1,190 +1,196 @@
-{-# LANGUAGE Arrows                     #-}
-{-# LANGUAGE BlockArguments             #-}
-{-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE DeriveFunctor              #-}
-{-# LANGUAGE DeriveGeneric              #-}
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE Arrows #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase                 #-}
-{-# LANGUAGE NoImplicitPrelude          #-}
-{-# LANGUAGE NoMonomorphismRestriction  #-}
-{-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE ScopedTypeVariables        #-}
-{-# LANGUAGE StandaloneDeriving         #-}
-{-# LANGUAGE TemplateHaskell            #-}
-{-# LANGUAGE TupleSections              #-}
-{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 
 module Pipes.XML.Token where
 
-import           Control.Arrow                    ((***))
-import           Control.Lens                     hiding (deep, none)
-import           Control.Lens.Extras
-import           Control.Monad.Cont
-import           Data.Attoparsec.ByteString.Char8
-import qualified Data.ByteString.Builder          as B
-import qualified Data.ByteString.Char8            as B
-import           Data.Map                         (Map, insert)
-import qualified Data.Text                        as T
-import qualified Data.Text.Encoding               as T
-import           Pipes
-import           Pipes.Core
-import qualified Pipes.Prelude                    as P
-import           Pipes.Safe                       hiding (handle)
-import           Protolude
-
+import Control.Lens hiding (deep, none)
+import Control.Monad.Cont
+import Data.Attoparsec.ByteString.Char8
+import qualified Data.ByteString.Char8 as B
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import Pipes
+import Protolude
 
 -- | xml tokens
-data Token = Tin Text -- ^ tag starts
-           | TinC Text -- ^  end of attribute list
-           | Tattr Text ByteString -- ^ an atttribute
-           | Tout Text -- ^ tag ends
-           | Ttext ByteString -- ^ some text
-           | Tcdata ByteString -- ^ some CDATA -- not implemented
-           | Tcomment Text
-           | Tpreproc Text ByteString
-           | Tdtd Text (Either ByteString [ByteString])
-    deriving Show
+data Token
+  = -- | tag starts
+    Tin Text
+  | -- |  end of attribute list
+    TinC Text
+  | -- | an atttribute
+    Tattr Text ByteString
+  | -- | tag ends
+    Tout Text
+  | -- | some text
+    Ttext ByteString
+  | -- | some CDATA -- not implemented
+    Tcdata ByteString
+  | Tcomment Text
+  | Tpreproc Text ByteString
+  | Tdtd Text (Either ByteString [ByteString])
+  deriving (Show)
 
 makePrisms ''Token
 
-chunking
-    :: (Show s, Show a, Functor m)
-    => s -- ^ initial parser state
-    -> (s -> Parser (s, [a])) -- ^ output stream item parse
-    -> Pipe ByteString a m ()
+chunking ::
+  (Show s, Show a, Functor m) =>
+  -- | initial parser state
+  s ->
+  -- | output stream item parse
+  (s -> Parser (s, [a])) ->
+  Pipe ByteString a m ()
 chunking s0 p = go (parse $ p s0)
   where
     go q = do
-        let loop q b = case q b of
-                Done r (s, xs) -> do
-                    traverse_ yield xs
-                    if not . B.null $ r
-                        then loop (parse $ p s) r
-                        else go $ parse $ p s
-                f@(Partial _) -> go $ feed f
-                x             -> panic $ show x
-        await >>= loop q
+      let loop q' b = case q' b of
+            Done r (s, xs) -> do
+              traverse_ yield xs
+              if not . B.null $ r
+                then loop (parse $ p s) r
+                else go $ parse $ p s
+            f@(Partial _) -> go $ feed f
+            x -> panic $ show x
+      await >>= loop q
 
-data StateP =  Outside  | Attring Text | Elements Text deriving Show
+data StateP = Outside | Attring Text | Elements Text deriving (Show)
 
 dtdElements :: Parser [ByteString]
 dtdElements = do
-    char '['
-    xs <- many dtdElement
-    skipSpace
-    char ']' 
-    pure xs
+  void $ char '['
+  xs <- many dtdElement
+  skipSpace
+  void $ char ']'
+  pure xs
 
-dtdElement :: Parser ByteString 
+dtdElement :: Parser ByteString
 dtdElement = do
-    skipSpace
-    string "<!"
-    skipSpace
-    parseString '>' <* char '>'
+  skipSpace
+  void $ string "<!"
+  skipSpace
+  parseString '>' <* char '>'
 
 -- ts = traceShow
 
-parseToken :: StateP -> Parser ( StateP, [ Token ] )
-parseToken Outside = msum
-    [  do
-          string "</"
-          name <- parseString '>' <* char '>'
-          pure ( Outside, pure $ Tout $ T.strip $ toS name )
-    , do
-          string "<!--"
-          skipSpace
-          c <- T.decodeUtf8 <$> parseStringS "-->" 
-          pure ( Outside, pure $ Tcomment c)
-    , do
-          string "<?"
-          skipSpace
-          n <- T.decodeUtf8 <$> parseString ' '
-          skipSpace
-          c <- parseStringS "?>" 
-          pure ( Outside, pure $ Tpreproc n c)
-    , do
-          string "<!"
-          skipSpace
-          string "DOCTYPE"
-          skipSpace
-          n <- T.decodeUtf8 <$> parseString ' '
-          skipSpace
-          xs <- Right <$> dtdElements -- <|>  Left <$> parseQuoted
-          skipSpace 
-          char '>'
-          pure (Outside, pure $ Tdtd n xs)
-
-    , do
-          char '<' 
-          skipSpace
-          (n, d) <- parseStringP (0 <$ char ' ' <|> 1 <$ char  '>' <|> 2 <$ string "/>")
-          let name = T.decodeUtf8 n
-          case d of
-              0 -> pure ( Attring name, pure $ Tin name )
-              1 -> pure ( Outside, [Tin name, TinC name] )
-              2 -> pure ( Outside, [Tin name, TinC name, Tout name])
-    , do
-          t <- parseString '<'
-          guard $ B.length t > 0
-          '<' <- peekChar'
-          pure (Outside, pure $ Ttext t)
-    
+parseToken :: StateP -> Parser (StateP, [Token])
+parseToken Outside =
+  msum
+    [ do
+        void $ string "</"
+        name <- parseString '>' <* char '>'
+        pure (Outside, pure $ Tout $ T.strip $ toS name),
+      do
+        void $ string "<!--"
+        skipSpace
+        c <- T.decodeUtf8 <$> parseStringS "-->"
+        pure (Outside, pure $ Tcomment c),
+      do
+        void $ string "<?"
+        skipSpace
+        n <- T.decodeUtf8 <$> parseString ' '
+        skipSpace
+        c <- parseStringS "?>"
+        pure (Outside, pure $ Tpreproc n c),
+      do
+        void $ string "<!"
+        skipSpace
+        void $ string "DOCTYPE"
+        skipSpace
+        n <- T.decodeUtf8 <$> parseString ' '
+        skipSpace
+        xs <- Right <$> dtdElements -- <|>  Left <$> parseQuoted
+        skipSpace
+        void $ char '>'
+        pure (Outside, pure $ Tdtd n xs),
+      do
+        void $ char '<'
+        skipSpace
+        (n, d :: Int) <- parseStringP (0 <$ char ' ' <|> 1 <$ char '>' <|> 2 <$ string "/>")
+        let name = T.decodeUtf8 n
+        case d of
+          0 -> pure (Attring name, pure $ Tin name)
+          1 -> pure (Outside, [Tin name, TinC name])
+          2 -> pure (Outside, [Tin name, TinC name, Tout name])
+          _ -> panic "parse token is partial",
+      do
+        t <- parseString '<'
+        guard $ B.length t > 0
+        '<' <- peekChar'
+        pure (Outside, pure $ Ttext t)
     ]
 parseToken (Attring name) = do
-    skipSpace
-    msum [ do
-               () <$ char '>' <|> () <$ string "?>"
-               pure ( Outside, pure $ TinC name )
-         , do
-               () <$ string "/>"
-               pure ( Outside, [ TinC name, Tout name ] )
-         , do
-               attr <- takeWhile1 ('=' /=)
-               char '='
-               skipSpace
-               value <- parseQuoted
-               pure ( Attring name
-                    , pure $ Tattr (T.strip $ T.decodeUtf8 attr) value
-                    )
-         ]
+  skipSpace
+  msum
+    [ do
+        () <$ char '>' <|> () <$ string "?>"
+        pure (Outside, pure $ TinC name),
+      do
+        () <$ string "/>"
+        pure (Outside, [TinC name, Tout name]),
+      do
+        attr <- takeWhile1 ('=' /=)
+        void $ char '='
+        skipSpace
+        value <- parseQuoted
+        pure
+          ( Attring name,
+            pure $ Tattr (T.strip $ T.decodeUtf8 attr) value
+          )
+    ]
+parseToken _ = panic "parse token ins partial"
 
 escape :: Parser Char
 escape = char '\\' >> '"' <$ satisfy (== '"')
 
 amp :: Parser Char
 amp = do
-    char '&'
-    x <- msum [ '<' <$ string "lt"
-              , '>' <$ string "gt"
-              , '&' <$ string "amp"
-              , '"' <$ string "quot"
-              , '\'' <$ string "apos"
-              ]
-    char ';'
-    pure x
+  void $ char '&'
+  x <-
+    msum
+      [ '<' <$ string "lt",
+        '>' <$ string "gt",
+        '&' <$ string "amp",
+        '"' <$ string "quot",
+        '\'' <$ string "apos"
+      ]
+  void $ char ';'
+  pure x
 
 character :: Char -> Parser Char
-character c = amp <|>  escape <|> satisfy (/= c)
+character c = amp <|> escape <|> satisfy (/= c)
 
-characterS :: Parser a -> Parser ([Char],a)
-characterS c = (mempty,) <$> c 
-    <|>  first . (:) <$>  (amp <|>  escape <|>  anyChar) <*> characterS c
+characterS :: Parser a -> Parser ([Char], a)
+characterS c =
+  (mempty,) <$> c
+    <|> first . (:) <$> (amp <|> escape <|> anyChar) <*> characterS c
 
 parseString :: Char -> Parser ByteString
 parseString c = B.pack <$> many (character c)
 
-parseStringS :: ByteString -> Parser ByteString 
-parseStringS c = fst  <$> parseStringP (string c)
+parseStringS :: ByteString -> Parser ByteString
+parseStringS c = fst <$> parseStringP (string c)
 
 parseStringP :: Parser a -> Parser (ByteString, a)
-parseStringP c = first B.pack  <$> characterS c
+parseStringP c = first B.pack <$> characterS c
 
 parseQuoted :: Parser ByteString
 parseQuoted = do
-    char '"'
-    x <- parseString '"'
-    char '"'
-    pure x
-
+  void $ char '"'
+  x <- parseString '"'
+  void $ char '"'
+  pure x
